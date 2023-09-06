cda4084fbe83f27823480e8653af7a1249e80e49
Donald <docar.decz@gmail.com>
2023-09-05 23:37:39 -0400

    Creacion de Rutas login y register

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 import express from "express";
-
+import authRoutes from './routes/auth.routes.js'
 const app = express();
+
+app.use('/api',authRoutes)
 
 export default app;
---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,4 @@
+export const register = (req,res)=>{
+    console.log(red.body)
+};
+export const login = (req,res)=>{};
